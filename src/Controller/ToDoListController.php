7663ecc53350879bdd90c5ac928bf917df43429a
ilkisha <i.shalvarkov@gmail.com>
2020-06-27 15:27:40 +0300

    Added methods in controller for CRUD operations

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -14,4 +14,30 @@ class ToDoListController extends AbstractController
     {
         return $this->render('index.html.twig');
     }
+
+    /**
+     * @Route("/create", name="create_task", methods={"POST"})
+     */
+    public function create()
+    {
+        exit('to do: create a new task!');
+    }
+
+    /**
+     * @Route("/switch-status/{id}", name="switch_status")
+     * @param int $id
+     */
+    public function switchStatus(int $id): void
+    {
+        exit('to do: Switch status of the task!' . $id);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="task_delete")
+     * @param int $id
+     */
+    public function delte(int $id): void
+    {
+        exit('to do: Delete a task with the id of ' . $id);
+    }
 }
